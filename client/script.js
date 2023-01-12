4eea06e5418b3c43c2ddc67a2d7087cfd3a951c6
mihai-m-and-m <mihai00792@gmail.com>
2023-01-12 16:31:37 +0200

    external server added

--- client/script.js
+++ client/script.js
@@ -63,7 +63,7 @@ const handleSubmit = async e => {
 
 	loader(messageDiv);
 
-	const response = await fetch("http://localhost:5000/", {
+	const response = await fetch("https://ai-app-vqhv.onrender.com/", {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({ prompt: data.get("prompt") }),
